7eee9db0d1d1c7674cdfe1e08d71c5c44f98d4b7
Viktoria <viktoria.titova15@yandex.ru>
2019-04-25 21:00:07 +0100

    add the resolve storage class to the DI configuration with a selection from appsettings.json and additional enum

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using LunTi.Models;
+using LunTi.Storage;
 
 namespace LunTi
 {
@@ -26,6 +28,18 @@ namespace LunTi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+           switch (Configuration["Storage:Type"].ToStorageEnum())
+            {
+                case StorageEnum.MemCache:
+                    services.AddSingleton<IStorage<Lab1Data>, MemCache>();
+                    break;
+                case StorageEnum.FileStorage:
+                    services.AddSingleton<IStorage<Lab1Data>>(
+                        x => new FileStorage(Configuration["Storage:FileStorage:Filename"], int.Parse(Configuration["Storage:FileStorage:FlushPeriod"])));
+                    break;
+                default:
+                    throw new IndexOutOfRangeException($"Storage type '{Configuration["Storage:Type"]}' is unknown");
+            } 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
